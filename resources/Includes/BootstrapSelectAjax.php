bc759822afb9d93d1a581314e1fee915b8260a4d
pmirand6 <pmirand6@gmail.com>
2020-03-19 20:57:07 -0300

    Se implementa el plugin de lenguaje español para el plugin de Select Ajax

--- resources/Includes/BootstrapSelectAjax.php
+++ resources/Includes/BootstrapSelectAjax.php
@@ -8,5 +8,8 @@
 
 echo "
     <!-- Bootstrap Select Ajax-->
-        <script type=\"text/javascript\" language=\"javascript\" src=\"" . HTTP ."/resources/Bootstrap-Select-Ajax/js/ajax-bootstrap-select.min.js\"></script>";
+        <script type=\"text/javascript\" language=\"javascript\" src=\"" . HTTP ."/resources/Bootstrap-Select-Ajax/js/ajax-bootstrap-select.min.js\"></script>
+    <!-- Lenguaje Español de Select Ajax -->
+    <script type=\"text/javascript\" language=\"javascript\" src=\"" . HTTP ."/resources/Bootstrap-Select-Ajax/js/locale\ajax-bootstrap-select.es-ES.js\"></script>
+    ";
 ?>
