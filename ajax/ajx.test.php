e2187e21a182686f35a093c329d09b9d5ea888a6
pmirand6 <pmirand6@gmail.com>
2020-04-14 22:49:32 -0300

    comienzo de implementacion de la clase mail

--- ajax/ajx.test.php
+++ ajax/ajx.test.php
@@ -1,68 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
+/*require_once("../config/config.php");
+include $_SERVER['DOCUMENT_ROOT'] . _BD;
+include $_SERVER['DOCUMENT_ROOT'] . _FN;*/
 
-<div class="easyui-layout" data-options="fit:true">
-   <div data-options="region:'north'" style="height:10%" class="head_barra col-sm-12 col-xs-12">
-      <div class="col-sm-1 col-xs-1">
-         <img src="resources/IMG/soliris.svg" class="logo img-rounded">
-      </div>
-      <div class="col-sm-9 col-xs-8">
-         <p id="info" style="text-align:right;vertical-align: middle;"></p>
-      </div>
-      <!--      Botones de usuario y LogOut      -->
-      <div class="col-sm-2 col-xs-3 text-right" id="botonesHeader">
-         <div class="btn-group" role="group">
-            <button type="button" id="user" class="btn btn-info pointer fa fa-user" data-container="body" data-toggle="popover">
-            <b style="margin-left: 10px">PMIRANDA</b>
-            </button>
-         </div>
-         <div id="popoverUser" class="hidden">
-            <div>
-               <form id="frm_grupo">
-                  <span>Cambiar Grupo: </span>
-                  <div class="form-check">
-                     <label class="form-check-label">
-                     <input class="form-check-input" type="radio" value="ventas" onclick="change_location(this.value);" >
-                     Ventas 
-                     </label>
-                  </div>
-                  <div class="form-check">
-                     <label class="form-check-label">
-                     <input class="form-check-input" type="radio" value="fv" onclick="change_location(this.value);" >
-                     FV
-                     </label>
-                  </div>
-                  <div class="form-check">
-                     <label class="form-check-label">
-                     <input class="form-check-input" type="radio" value="marketing" onclick="change_location(this.value);" >
-                     MKT
-                     </label>
-                  </div>
-               </form>
-               <p><b>Usuario: pmiranda</b></p>
-               <p><b>Grupo: admin</b></p>
-               <p class="text-right">
-                  <a class="btn btn-default pointer fa fa-history" title="Historial" id="history" onclick="viewHistory();"></a>
-                  <a class="btn btn-danger pointer fa fa-power-off" title="Desloguearse" id="logoff" onclick="deleteProfile();"></a>
-               </p>
-            </div>
-         </div>
-      </div>
-   </div>
-   <!--    Navegador   -->
-   <div class=" head_barra col-sm-1 col-xs-2" data-options="region:'west',split:true,title:'Navegador'" href="nav.php" id="nav"></div>
-   <!--    Principal   -->
-   <div class="col-sm-11 col-xs-10" data-options="region:'center'" id="main" style="width: 100%; height: 100% ; overflow: hidden">
-      <iframe id="if_main" src="" marginheight="0" marginwidth="0" noresize frameborder="0"></iframe>
-   </div>
-</div>
-    
-</body>
-</html>
+require '../clases/Mail.php';
+
+//Now creating and sending a message becomes simpler when you use this class in your app code
+try {
+   //Instantiate your new class, making use of the new `$body` parameter
+   $mail = new Mail(true, '<strong>This is the message body</strong>');
+   // Now you only need to set things that are different from the defaults you defined
+   $mail->addAddress('jane@example.com', 'Jane User');
+   $mail->Subject = 'Here is the subject';
+   //$mail->addAttachment(__FILE__, 'myPHPMailer.php');
+   $mail->send(); //no need to check for errors - the exception handler will do it
+} catch (Exception $e) {
+   //Note that this is catching the PHPMailer Exception class, not the global \Exception type!
+   echo 'Caught a '. get_class($e) .': '. $e->getMessage();
+}
---
+++ clases/Mail.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This example shows how to extend PHPMailer to simplify your coding.
+ * If PHPMailer doesn't do something the way you want it to, or your code
+ * contains too much boilerplate, don't edit the library files,
+ * create a subclass instead and customise that.
+ * That way all your changes will be retained when PHPMailer is updated.
+ */
+
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+require '../vendor/autoload.php';
+
+/**
+ * Use PHPMailer as a base class and extend it
+ */
+class Mail extends PHPMailer
+{
+    /**
+     * myPHPMailer constructor.
+     *
+     * @param bool|null $exceptions
+     * @param string    $body A default HTML message body
+     */
+    public function __construct($exceptions, $body = '')
+    {
+        //Don't forget to do this or other things may not be set correctly!
+        parent::__construct($exceptions);
+        //Set a default 'From' address
+        $this->setFrom('cron@raffo.com.ar', 'Cron');
+        //Send via SMTP
+        $this->isSMTP();
+        //Equivalent to setting `Host`, `Port` and `SMTPSecure` all at once
+        $this->Host = 'tls://smtp.example.com:587';
+        //Set an HTML and plain-text body, import relative image references
+        $this->msgHTML($body, './images/');
+        //Show debug output
+        $this->SMTPDebug = SMTP::DEBUG_SERVER;
+        //Inject a new debug output handler
+        $this->Debugoutput = static function ($str, $level) {
+            echo "Debug level $level; message: $str\n";
+        };
+    }
+
+    //Extend the send function
+    public function send()
+    {
+        $this->Subject = '[Yay for me!] ' . $this->Subject;
+        $r = parent::send();
+        echo 'I sent a message with subject '. $this->Subject;
+
+        return $r;
+    }
+}
+
