cb5875a932344c776cfdabc8d89557fa41193c19
Miranda <pmiranda@raffo.com.ar>
2020-04-15 15:11:52 -0300

    Configuracion de clase

--- clases/MailNotificaciones.php
+++ clases/MailNotificaciones.php
@@ -1,23 +1,9 @@
 <?php
 
-/**
- * This example shows how to extend PHPMailer to simplify your coding.
- * If PHPMailer doesn't do something the way you want it to, or your code
- * contains too much boilerplate, don't edit the library files,
- * create a subclass instead and customise that.
- * That way all your changes will be retained when PHPMailer is updated.
- */
 
-//Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\PHPMailer;
 
 require '../vendor/autoload.php';
 
-/**
- * Use PHPMailer as a base class and extend it
- */
 class MailNotificaciones
 {
     public static function sendMessage($subject, $body, Array $destinatarios)
@@ -31,10 +17,10 @@ class MailNotificaciones
         $mailer = new Swift_Mailer($transport);
 
         // Create a message
-        $message = (new Swift_Message('Wonderful Subject'))
+        $message = (new Swift_Message($subject))
             ->setFrom(['cron@raffo.com.ar' => 'Cron'])
-            ->setTo(['pmiranda@raffo.com.ar'])
-            ->setBody('Here is the message itself');
+            ->setTo($destinatarios)
+            ->setBody($body);
 
         // Send the message
         $result = $mailer->send($message);
