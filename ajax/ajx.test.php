8f6573c5a7bde36a2da72b08c7a031c407e71668
pmirand6 <pmirand6@gmail.com>
2020-04-20 09:14:31 -0300

    actualizacion del repo

--- ajax/ajx.test.php
+++ ajax/ajx.test.php
@@ -1,10 +1,24 @@
 <?php
 
-require_once("../config/config.php");
-include $_SERVER['DOCUMENT_ROOT'] . _MAIL;
+require '../clases/claseSuma.php';
 
 
-sendMail_AltaMedico('3', 'pmiranda');
+//Instancio la clase
+$auto = new Auto('variable color', 'constructor');
+
+echo $auto->sumaInterfaz('1', '2');
+
+
+/*echo 'los valores por defecto son: ' . $auto->getColorAuto(); 
+
+echo '</br>';
+//llamo al metodo setColorAuto
+echo $auto->setColorAuto('desde', 'el metodo');
+
+// llamo al metodo getColorAuto
+echo 'los valores cambiados son: ' . $auto->getColorAuto();*/ 
+
+
 
 
 
