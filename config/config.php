a3a8aa6b9113254c90f8f38d47a57ded91ff56b8
Miranda <pmiranda@raffo.com.ar>
2020-04-26 18:39:05 -0300

    update de configuracion

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php 
+
+// DEFINICION DE CONSTANTES
+
+$nombreApp = explode(DIRECTORY_SEPARATOR, __DIR__);
+
+define("_NAME", $nombreApp[3]);
+define("_NAME_NP", 'nota_pedido');
+define("_BD", "/"  . _NAME . "/base.php");
+define("_FN", "/"  . _NAME . "/resources/PHP/funciones.php");
+define("_SG", "/"  . _NAME . "/resources/PHP/accesos_seguridad.php");
+define("_MAIL","/"  . _NAME . "/helpers/mailFunciones.php");
+define("HTTP", 'http://' . $_SERVER['HTTP_HOST'] . '/' . _NAME);
+define("_RESOURCES", _NAME . '/resources/');
+define("_ADMIN_MAIL", 'pmiranda@raffo.com.ar');
+define("_DOC_VENTAS", HTTP . '/documentacion/venta/');
+define("_URL_SERVIDOR", '192.168.0.139');
+define("_USUARIO_BD", 'soliris_ap');
+define("_PASSWORD_BD", 'acceso_ap');
+define("_TABLA_BD", 'soliris');
+
---
+++ resources/PHP/db.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sogvicious
+ * Date: 25/07/2015
+ * Time: 08:27 PM
+ */
+
+include_once realpath(dirname(__FILE__)."/../../") . '/config/config.php';
+
+//echo basename($_SERVER['SCRIPT_FILENAME']);
+    // TODO manejo de conexiones
+    //$db = mysqli_connect('192.168.0.62','soliris_ap','acceso_app') or die ("Database error"); // conector produccion
+    $db = mysqli_connect('192.168.0.139','soliris_ap','acceso_app') or die ("Database error"); // conector desarrollo
+    //$db = mysqli_connect('127.0.0.1','admin','') or die ("Database error"); // conector desarrollo local
+    mysqli_set_charset($db, 'utf8');
+    mysqli_select_db($db, _NAME);
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    /*$db_np = mysqli_connect('192.168.0.47','soliris_ap','soliris') or die ("Database error");
+    mysqli_set_charset($db, 'utf8');
+    mysqli_select_db($db, _NAME_NP);
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }*/
+
+    
+    
+?>
